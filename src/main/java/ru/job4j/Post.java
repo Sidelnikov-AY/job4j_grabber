fd2b92e11b817fd3f8f7cd8f80b2300f9ebc0557
asidelnikov <a@a.ru>
2021-11-14 00:31:52 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/Post.java
+++ src/main/java/ru/job4j/Post.java
@@ -8,7 +8,15 @@ public class Post {
     private String title;
     private String link;
     private String description;
-    private LocalDateTime localDateTime;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
 
     @Override
     public String toString() {
@@ -17,7 +25,7 @@ public class Post {
                 + ", title='" + title + '\''
                 + ", link='" + link + '\''
                 + ", description='" + description + '\''
-                + ", localDateTime=" + localDateTime
+                + ", localDateTime=" + created
                 + '}';
     }
 
@@ -41,7 +49,7 @@ public class Post {
         if (link != null ? !link.equals(post.link) : post.link != null) {
             return false;
         }
-        return localDateTime != null ? localDateTime.equals(post.localDateTime) : post.localDateTime == null;
+        return created != null ? created.equals(post.created) : post.created == null;
     }
 
     @Override
@@ -49,7 +57,47 @@ public class Post {
         int result = id;
         result = 31 * result + (title != null ? title.hashCode() : 0);
         result = 31 * result + (link != null ? link.hashCode() : 0);
-        result = 31 * result + (localDateTime != null ? localDateTime.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
         return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,21 +4,54 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.Post;
 import ru.job4j.utils.SqlRuDateTimeParser;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SqlRuParse {
+
+    public Post postDetail(int id, String title, String link, String description, LocalDateTime created) {
+        return new Post(id, title, link, description, created);
+    }
+
     public static void main(String[] args) throws Exception {
+        List<Post> postList = new ArrayList<>();
+        SqlRuParse srp = new SqlRuParse();
+        int posts = 0;
         for (int page = 1; page <= 5; page++) {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            Element parent = td.parent();
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
-            System.out.println(timeParser.parse(parent.child(5).text()));
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Element href = td.child(0);
+                    Element parent = td.parent();
+                    System.out.println(href.attr("href"));
+                    System.out.println(href.text());
+                    SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
+                    System.out.println(timeParser.parse(parent.child(5).text()));
+                    Document post = Jsoup.connect(href.attr("href")).get();
+                    Elements table = post.select(".msgTable");
+                    for (Element td2 : table) {
+                        Element description = td2.child(0).child(1).child(1);
+                        System.out.println(description.text());
+                        Element date = td2.child(0).child(2).child(0);
+                        String[] dateArr = date.text().split("\\[");
+                        System.out.println(timeParser.parse(dateArr[0].trim()));
+
+                        postList.add(srp.postDetail(posts++, href.text(), href.attr("href"),
+                                description.text(), timeParser.parse(dateArr[0].trim())));
+
+                        break;
+                    }
+                }
+            }
+
+        for (Post post: postList) {
+            System.out.println(post.toString());
+
         }
-        }
+        System.out.println(postList.size());
     }
 }
