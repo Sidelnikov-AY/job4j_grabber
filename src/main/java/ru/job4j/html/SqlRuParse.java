7e47148da02f8372d25d43e4c941d96476aff400
asidelnikov <a@a.ru>
2021-11-28 12:52:47 +0500

    2.4. SqlRuParse [#285213 #229354]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -24,39 +24,49 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
+        try {
         for (int page = 1; page <= 5; page++) {
             Document doc = Jsoup.connect(link + page).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 String hrefString = td.child(0).attr("href");
-                posts.add(detail(hrefString));
+                if (hrefString.contains("java") || hrefString.contains("Java")) {
+                    posts.add(detail(hrefString));
+                }
             }
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return posts;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
+    public Post detail(String link) {
         Post rsl = null;
         SqlRuParse srp = new SqlRuParse(dateTimeParser);
-        Document post = Jsoup.connect(link).get();
-        Elements table = post.select(".msgTable");
-        for (Element td2 : table) {
-            Element description = td2.child(0).child(1).child(1);
-            Element title = td2.child(0).child(0).child(0);
-            Element date = td2.child(0).child(2).child(0);
-            String[] dateArr = date.text().split("\\[");
-            rsl = new Post(0, title.text(), link,
-                    description.text(), srp.dateTimeParser.parse(dateArr[0].trim()));
-            break;
-        }
+        try {
+            Document post = Jsoup.connect(link).get();
+            Elements table = post.select(".msgTable");
+            for (Element td2 : table) {
+                Element description = td2.child(0).child(1).child(1);
+                Element title = td2.child(0).child(0).child(0);
+                Element date = td2.child(0).child(2).child(0);
+                String[] dateArr = date.text().split("\\[");
+                rsl = new Post(0, title.text(), link,
+                        description.text(), srp.dateTimeParser.parse(dateArr[0].trim()));
+                break;
+            }
+        } catch (IOException e) {
+                e.printStackTrace();
+            }
         return rsl;
     }
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         DateTimeParser dtp = new SqlRuDateTimeParser();
         SqlRuParse srp = new SqlRuParse(dtp);
         srp.posts = srp.list("https://www.sql.ru/forum/job-offers/");
